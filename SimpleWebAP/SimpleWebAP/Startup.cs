0a50dcc101ef7ffce9fc7f90dd61ddb647f321d4
Thummati <Thummati@DESKTOP-AA3MR65>
2018-07-06 18:58:09 +0530

    Checking in the SimpleWebAPP for Thummati and Jagdessh POC

---
+++ SimpleWebAP/SimpleWebAP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleWebAP.Startup))]
+namespace SimpleWebAP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
